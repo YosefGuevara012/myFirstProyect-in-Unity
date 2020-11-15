55051f5c3b03a546337d68baf341479a5a226e35
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-11-15 01:25:58 -0500

    first commit

---
+++ myFirstScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myFirstScript : MonoBehaviour
+{
+
+    public int carDoors = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log(4 + 5);
+
+        Debug.Log(carDoors + 3);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
