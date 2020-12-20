8423ce02c54d930f2fe30414f530343d41333d7f
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-12-20 03:09:19 -0500

    Starting OOP - cap 6 Structures

--- Character.cs
+++ Character.cs
@@ -2,6 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public struct Weapon
+{
+    public string name;
+    public int damage;
+
+    public Weapon(string name, int damage)
+    {
+
+        this.name = name;
+        this.damage = damage;
+
+    }
+
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Weapon name: {0} - {1}", this.name, this.damage);
+    }
+}
+
 public class Character
 {
 
--- myFirstScript.cs
+++ myFirstScript.cs
@@ -7,17 +7,45 @@ using UnityEngine;
 
 public class myFirstScript : MonoBehaviour
 {
-    
 
-    
+
+
     void Start()
     {
 
+        // Classes called with the same name, point to the same memory place
         Character hero = new Character();
         hero.PrintCharacterStatus();
 
         Character heroine = new Character("Lara Croft");
         heroine.PrintCharacterStatus();
+
+        // example
+        Character hero2 = hero;
+        hero2.PrintCharacterStatus();
+
+        // If you change one of them
+        hero2.name = "Carlos";
+
+        // Result
+        hero.PrintCharacterStatus();
+        hero2.PrintCharacterStatus();
+
+
+        // It does not happen the same with the Structures
+        // Because does not point to the same memory place
+
+        Weapon sword = new Weapon("Wood Sword", 5);
+        Weapon sword2 = sword;
+
+
+        sword2.name = "Excalibur";
+        sword2.damage = 1000;
+
+        sword.PrintWeaponStats();
+        sword2.PrintWeaponStats();
+
+
         
 
     }
