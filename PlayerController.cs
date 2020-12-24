5c9e76180ae9ba4ac71f39053d73d245fa34e692
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-12-24 16:23:58 -0500

    Player Controller script added

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    // Variables
+
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 60f;
+
+    private float hInput, vInput;
+
+
+    // Methods
+
     // Start is called before the first frame update
     void Start()
     {
+
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // How much is going to move (Hardware Speed)
+        hInput = Input.GetAxis("Horizontal") * rotateSpeed;
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
+
+        // Movement Vector (Phisical Speed)
+
+        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+
+
+
     }
 }
