5674d3bcbdce3c0f85243fc54fccc38f42414757
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-12-29 10:24:31 -0500

    Manager script configured to set the HP and the number of items

--- GameManager.cs
+++ GameManager.cs
@@ -5,9 +5,46 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-    private int _itemsCollected = 0;
-    private int _playerHP = 100;
 
+  
+    // added some logic to get or set the variables to our data
+
+    private int _itemsCollected = 0;
+    public int Items
+    {
+        get
+        {
+            // Aqui poneis codigo cuando se cosnsulta una variable
+            return _itemsCollected;
+
+        }
+        set
+        {
+            _itemsCollected = value;
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+        }
+    }
+
+    private int _playerHP = 0;
+    private int HP
+    {
+        get
+        {
+            return _playerHP;
+
+        }
+        set
+        {   
+            if (value >= 0 && value <= 3)
+            {
+
+               _playerHP = value;
+
+            }
+           
+            Debug.LogFormat("Vidas: {0}", _playerHP);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
