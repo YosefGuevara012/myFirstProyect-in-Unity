4d21ec067ee0608c6645eb26f0c5e6fc0ff53618
YosefGuevara012 <yosef.guevara012@hotmail.com>
2020-12-31 12:50:25 -0500

    Abstract classes

---
+++ BaseManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseManager
+{
+
+    protected string _state;
+
+    public abstract string State { get; set; }
+
+    public abstract void Initialize();
+}
---
+++ BattleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : BaseManager
+{
+
+    // Now it is possible to use the protected variable from
+    // the father (BaseManager)
+    public override string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
+    public override void Initialize()
+    {
+        _state = "Manager Inicializado";
+        Debug.Log(_state);
+    }
+
+}
